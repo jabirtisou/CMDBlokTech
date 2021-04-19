aaa709528a4cc5ef6c91263ee50c033827f296b0
Jabir Tisoudali <jabirtisou@hotmail.com>
2021-04-19 12:04:00 +0200

    data

---
+++ index.js
@@ -0,0 +1,45 @@
+const camelCase = require('camelcase');
+ 
+camelCase('foo-bar');
+//=> 'fooBar'
+ 
+camelCase('foo_bar');
+//=> 'fooBar'
+ 
+camelCase('Foo-Bar');
+//=> 'fooBar'
+ 
+camelCase('розовый_пушистый_единороги');
+//=> 'розовыйПушистыйЕдинороги'
+ 
+camelCase('Foo-Bar', {pascalCase: true});
+//=> 'FooBar'
+ 
+camelCase('--foo.bar', {pascalCase: false});
+//=> 'fooBar'
+ 
+camelCase('Foo-BAR', {preserveConsecutiveUppercase: true});
+//=> 'fooBAR'
+ 
+camelCase('fooBAR', {pascalCase: true, preserveConsecutiveUppercase: true}));
+//=> 'FooBAR'
+ 
+camelCase('foo bar');
+//=> 'fooBar'
+ 
+console.log(process.argv[3]);
+//=> '--foo-bar'
+camelCase(process.argv[3]);
+//=> 'fooBar'
+ 
+camelCase(['foo', 'bar']);
+//=> 'fooBar'
+ 
+camelCase(['__foo__', '--bar'], {pascalCase: true});
+//=> 'FooBar'
+ 
+camelCase(['foo', 'BAR'], {pascalCase: true, preserveConsecutiveUppercase: true})
+//=> 'FooBAR'
+ 
+camelCase('lorem-ipsum', {locale: 'en-US'});
+//=> 'loremIpsum'
