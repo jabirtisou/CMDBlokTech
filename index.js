90e5663d27ba458f9f8f294f715dbe24225b6727
Jabir Tisoudali <jabirtisou@hotmail.com>
2021-04-20 16:37:07 +0200

    Update

--- index.js
+++ index.js
@@ -1,45 +1,11 @@
-const camelCase = require('camelcase');
- 
-camelCase('foo-bar');
-//=> 'fooBar'
- 
-camelCase('foo_bar');
-//=> 'fooBar'
- 
-camelCase('Foo-Bar');
-//=> 'fooBar'
- 
-camelCase('розовый_пушистый_единороги');
-//=> 'розовыйПушистыйЕдинороги'
- 
-camelCase('Foo-Bar', {pascalCase: true});
-//=> 'FooBar'
- 
-camelCase('--foo.bar', {pascalCase: false});
-//=> 'fooBar'
- 
-camelCase('Foo-BAR', {preserveConsecutiveUppercase: true});
-//=> 'fooBAR'
- 
-camelCase('fooBAR', {pascalCase: true, preserveConsecutiveUppercase: true});
-//=> 'FooBAR'
- 
-camelCase('foo bar');
-//=> 'fooBar'
- 
-console.log(process.argv[3]);
-//=> '--foo-bar'
-camelCase(process.argv[3]);
-//=> 'fooBar'
- 
-camelCase(['foo', 'bar']);
-//=> 'fooBar'
- 
-camelCase(['__foo__', '--bar'], {pascalCase: true});
-//=> 'FooBar'
- 
-camelCase(['foo', 'BAR'], {pascalCase: true, preserveConsecutiveUppercase: true})
-//=> 'FooBAR'
- 
-camelCase('lorem-ipsum', {locale: 'en-US'});
-//=> 'loremIpsum'
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello world !')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
