94bfa831657d9a08d7317090f4f649e08462f82f
Jabir Tisoudali <jabirtisou@hotmail.com>
2021-04-29 01:51:09 +0200

    update

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.send('Hello world !')
+})
+
+app.get('/:fullName/:slug', (req, res) => {
+  res.send(`<h1>${req.params.fullName}</h1>`)
+})
+
+app.use(function (req, res, next){
+  res.status(404).send("Sorry can't find that person.")
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
