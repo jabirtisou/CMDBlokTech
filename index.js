b121a9b8207a31f78eeb6c1c2138574fe22360e6
Jabir Tisoudali <jabirtisou@hotmail.com>
2021-05-14 14:46:05 +0200

    update

--- index.js
+++ index.js
@@ -5,21 +5,45 @@ const port = 3000
 
 
 const users = [{
+  "id": 6,
   "firstName": "Anna",
   "lastName": "Alexandria",
   "age" : 22,
   "description" : "Ik hou van sporten, en ik ontwerp kleren in mijn vrije tijd",
 },
 {
+  "id": 5,
   "firstName": "Kim",
   "lastName": "de Vries",
   "age" : 19,
   "description" : "Ik zoek een leuke vriend",
 },
 {
+  "id": 4,
   "firstName": "Sophie",
   "lastName": "Jansen",
   "age" : 23,
+  "description" : "Ik hou van sporten, en ik ontwerp kleren in mijn vrije tijd",
+},
+{
+  "id": 3,
+  "firstName": "Emerson",
+  "lastName": "Mcsweeney",
+  "age" : 30,
+  "description" : "Ik hou van sporten, en ik ontwerp kleren in mijn vrije tijd",
+}
+,{
+  "id": 2,
+  "firstName": "Ruth",
+  "lastName": "Williams",
+  "age" : 28,
+  "description" : "Ik hou van sporten, en ik ontwerp kleren in mijn vrije tijd",
+}
+,{
+  "id": 1,
+  "firstName": "Billy",
+  "lastName": "Hancock",
+  "age" : 26,
   "description" : "Ik hou van sporten, en ik ontwerp kleren in mijn vrije tijd",
 }
 ]
@@ -37,7 +61,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/findmatch', (req, res) => {
-  res.render('findmatch', {title:"find it"});
+  res.render('findmatch', {title:"find it", users});
 })
 app.get('/likelist', (req, res) => {
   res.render('likelist', {title:"My Like list"});
