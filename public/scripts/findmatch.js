0fbcda2ccd98a91c2590546fa963c8985b13fd31
Jabir Tisoudali <jabirtisou@hotmail.com>
2021-05-17 23:28:45 +0200

    css update

--- public/scripts/findmatch.js
+++ public/scripts/findmatch.js
@@ -1,18 +1,10 @@
-const main = document.getElementsByTagName('main')[1];
-const main2 = document.getElementsByTagName('main')[2];
-const main3 = document.getElementsByTagName('main')[3];
-const buttons = document.getElementsByClassName('buttons')[1];
-const buttons2 = document.getElementsByClassName('buttons')[2];
-const buttons3 = document.getElementsByClassName('buttons')[3];
+let allElements = document.querySelectorAll('.match');
+let allButtons = document.querySelectorAll('.buttons');
 
-
-// eslint-disable-next-line no-unused-vars
-function display() {
-  main.style.display = 'none';
-  main2.style.display = 'none';
-  main3.style.display = 'none';
-  buttons.style.display = 'none';
-  buttons2.style.display = 'none';
-  buttons3.style.display = 'none';
+function displayMain() {
+  for (let i = 1; i < allElements.length; i++) {
+    allElements[i].style.display = "none";
+    allButtons[i].style.display = "none"
+  }
 }
-display();
+displayMain();
--- public/scripts/models/User.js
+++ public/scripts/models/User.js
@@ -19,7 +19,7 @@ const UserSchema = new mongoose.Schema({
     },
     image: {
         type: String,
-        // required: true
+        required: true
     },
     likes: {
         type: Array,
