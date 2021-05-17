a4b15e395944fc010491e8722f9544f2b2d7c636
Jabir Tisoudali <jabirtisou@hotmail.com>
2021-05-17 20:14:45 +0200

    js

--- public/scripts/findmatch.js
+++ public/scripts/findmatch.js
@@ -1,1 +1,30 @@
-console.log('test')
+const main = document.getElementsByTagName('main')[1];
+const main2 = document.getElementsByTagName('main')[2];
+const main3 = document.getElementsByTagName('main')[3];
+const buttons = document.getElementsByClassName('buttons')[1];
+const buttons2 = document.getElementsByClassName('buttons')[2];
+const buttons3 = document.getElementsByClassName('buttons')[3];
+
+function display() {
+  main.style.display = 'none';
+  main2.style.display = 'none';
+  main3.style.display = 'none';
+  buttons.style.display = 'none';
+  buttons2.style.display = 'none';
+  buttons3.style.display = 'none';
+}
+
+display();
+
+
+// let i = array.length;
+// const array = document.getElementsByTagName('main')[i];
+// function display2() {
+//   if (i = 0) {
+//     console.log('Niks om te verbergen');
+//   } else {
+//     array[i].style.display = 'none';
+//   }
+// }
+
+// display2();
