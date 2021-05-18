7abef4d7ab94db02ebd332a87c9baa1dfc06af68
Jabir Tisoudali <jabirtisou@hotmail.com>
2021-05-18 22:24:23 +0200

    lint

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-/* eslint-disable no-console */
 const express = require('express');
-const cors = require("cors");
-const mongoose = require("mongoose");
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
+const cors = require('cors');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 const dotenv = require('dotenv');
-const User = require("./public/scripts/models/User")
-const isAuth = require("./public/scripts/middlewares/is-auth");
+const User = require('./public/scripts/models/User');
+const isAuth = require('./public/scripts/middlewares/is-auth');
 const app = express();
 dotenv.config();
 
@@ -14,33 +13,37 @@ const PORT = process.env.PORT || 3000;
 
 const store = new MongoDBStore({
   uri: 'mongodb+srv://jabirtisou:ikwilinloggen@cluster0.kotgm.mongodb.net/footballmatch',
-  collection: "sessions"
+  collection: 'sessions',
 });
 mongoose
-  .connect("mongodb+srv://jabirtisou:ikwilinloggen@cluster0.kotgm.mongodb.net/footballmatch", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log("MongoDB Connected..."))
-  .catch(err => console.log(err));
+  .connect(
+    'mongodb+srv://jabirtisou:ikwilinloggen@cluster0.kotgm.mongodb.net/footballmatch',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
 
 //Middlewares
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(cors());
 app.use(
   session({
-    secret: "my secret",
+    secret: 'my secret',
     resave: false,
     saveUninitialized: false,
-    store: store
+    store: store,
   })
 );
-
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   next();
@@ -50,217 +53,270 @@ app.use((req, res, next) => {
     return next();
   }
   User.findById(req.session.user._id)
-    .then(user => {
+    .then((user) => {
       if (!user) {
         return next();
       }
       req.user = user;
       next();
     })
-    .catch(err => {
+    .catch((err) => {
       next(new Error(err));
     });
 });
 
 //Routes
-app.get("/", home)
-app.post("/login", login)
-app.post("/user", addUser)
-app.get('/findmatch', isAuth, findMatch)
-app.post("/like/:userId", isAuth, likeUser)
-app.post("/reset/:userId", isAuth, resetUser)
-app.post("/dislike/:userId", isAuth, dislikeUser)
-app.get('/likelist', isAuth, likeList)
+app.get('/', home);
+app.post('/login', login);
+app.post('/user', addUser);
+app.get('/findmatch', isAuth, findMatch);
+app.post('/like/:userId', isAuth, likeUser);
+app.post('/reset/:userId', isAuth, resetUser);
+app.post('/dislike/:userId', isAuth, dislikeUser);
+app.get('/likelist', isAuth, likeList);
 app.get('/myprofile', isAuth, profile);
-
 //Routes functions
 function home(req, res) {
-  User.find().then(users => {
-    res.render('login', {
-      title: "Pick a user",
-      users
-    });
-  }).catch(err => console.log(err.message));
+  User.find()
+    .then((users) => {
+      res.render('login', {
+        title: 'Pick a user',
+        users,
+      });
+    })
+    .catch((err) => console.log(err.message));
 }
 
 function login(req, res, next) {
-  const {
-    user
-  } = req.body;
+  const { user } = req.body;
   req.session.isLoggedIn = true;
   req.session.user = user;
-  res.redirect("/findmatch")
+  res.redirect('/findmatch');
 }
 
 function addUser(req, res, next) {
-  const {
-    firstName,
-    lastName,
-    age,
-    description
-  } = req.body;
+  const { firstName, lastName, age, description } = req.body;
   const user = new User({
     firstName,
     lastName,
     age,
-    description
+    description,
   });
-  user.save().then(user => {
-    res.status(200).json(user);
-    // eslint-disable-next-line no-console
-  }).catch((err) => console.log(err.message));
+  user
+    .save()
+    .then((user) => {
+      res.status(200).json(user);
+      // eslint-disable-next-line no-console
+    })
+    .catch((err) => console.log(err.message));
 }
 
 function findMatch(req, res, next) {
-  console.log("Logged in user:", req.session.user)
+  console.log('Logged in user:', req.session.user);
   const userID = req.session.user;
-  User.findById(userID).then((loggedInUser) => {
-    const userLikes = [...loggedInUser.likes, ...loggedInUser.dislikes, userID];
-    console.log("userLikes", userLikes)
-    User.find({
-      _id: {
-        $nin: userLikes
-      }
-    }).then((users) => {
-      res.render('findmatch', {
-        title: "find it",
-        users
-      });
-    }).catch(err => err.message);
-  }).catch(err => console.log(err.message));
+  User.findById(userID)
+    .then((loggedInUser) => {
+      const userLikes = [
+        ...loggedInUser.likes,
+        ...loggedInUser.dislikes,
+        userID,
+      ];
+      console.log('userLikes', userLikes);
+      User.find({
+        _id: {
+          $nin: userLikes,
+        },
+      })
+        .then((users) => {
+          res.render('findmatch', {
+            title: 'find it',
+            users,
+          });
+        })
+        .catch((err) => err.message);
+    })
+    .catch((err) => console.log(err.message));
 }
 
 function likeUser(req, res) {
-  const {
-    likingID
-  } = req.body;
+  const { likingID } = req.body;
   const lUser = req.session.user;
-  User.findById(lUser).then((likingUser) => {
-    likingUser.likes.push(likingID);
-    likingUser.save().then(newLikingUser => {
-      const userId = req.params.userId;
-      User.findById(userId).then((likedUser) => {
-        likedUser.likedBy.push(lUser);
-        likedUser.save().then(newLikedUser => {
-          console.log("Liked User Updated!");
-          if (newLikedUser.likes.includes(lUser)) {
-            console.log("Match found!")
-            User.findByIdAndUpdate({
-              _id: userId
-            }, {
-              $pullAll: {
-                pending: [userId]
-              }
-            }, {
-              useFindAndModify: false
-            }).then((updatedLikedUser) => {
-              newLikingUser.matches.push(userId);
-              newLikingUser.matches.push(lUser);
-              newLikingUser.save().then((savedNewLikingUser) => {
-                updatedLikedUser.save().then((newUpdatedLikedUser) => {
-                  console.log("Update Made!")
-                });
-              }).catch(err => console.log(err));
-            }).catch(err => console.log(err));
-          } else {
-            console.log("pending...")
-            newLikingUser.pending.push(userId);
-            newLikingUser.save().then(() => {
-              console.log("pending update made");
-              res.redirect("/findmatch");
-            }).catch(err => console.log(err));
-          }
-        }).catch(err => console.log(err));
-      }).catch(err => console.log(err));
-      console.log("Liking User Updated!")
-    }).catch(err => console.log(err));
-  }).catch(err => console.log(err));
+  User.findById(lUser)
+    .then((likingUser) => {
+      likingUser.likes.push(likingID);
+      likingUser
+        .save()
+        .then((newLikingUser) => {
+          const userId = req.params.userId;
+          User.findById(userId)
+            .then((likedUser) => {
+              likedUser.likedBy.push(lUser);
+              likedUser
+                .save()
+                .then((newLikedUser) => {
+                  console.log('Liked User Updated!');
+                  if (newLikedUser.likes.includes(lUser)) {
+                    console.log('Match found!');
+                    User.findByIdAndUpdate(
+                      {
+                        _id: userId,
+                      },
+                      {
+                        $pullAll: {
+                          pending: [userId],
+                        },
+                      },
+                      {
+                        useFindAndModify: false,
+                      }
+                    )
+                      .then((updatedLikedUser) => {
+                        newLikingUser.matches.push(userId);
+                        newLikingUser.matches.push(lUser);
+                        newLikingUser
+                          .save()
+                          .then((savedNewLikingUser) => {
+                            updatedLikedUser
+                              .save()
+                              .then((newUpdatedLikedUser) => {
+                                console.log('Update Made!');
+                              });
+                          })
+                          .catch((err) => console.log(err));
+                      })
+                      .catch((err) => console.log(err));
+                  } else {
+                    console.log('pending...');
+                    newLikingUser.pending.push(userId);
+                    newLikingUser
+                      .save()
+                      .then(() => {
+                        console.log('pending update made');
+                        res.redirect('/findmatch');
+                      })
+                      .catch((err) => console.log(err));
+                  }
+                })
+                .catch((err) => console.log(err));
+            })
+            .catch((err) => console.log(err));
+          console.log('Liking User Updated!');
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
 }
 
 function resetUser(req, res) {
   const userId = req.params.userId;
   const loggedInUserId = req.session.user;
-  User.updateOne({
-    _id: loggedInUserId
-  }, {
-    $pullAll: {
-      likes: [userId]
-    }
-  }).then(() => {
-    User.updateOne({
-      _id: loggedInUserId
-    }, {
+  User.updateOne(
+    {
+      _id: loggedInUserId,
+    },
+    {
       $pullAll: {
-        pending: [userId]
-      }
-    }).then(() => {
-      console.log("successfully reset");
-      res.redirect("/findmatch");
-    }).catch(err => console.log(err));
-  }).catch(err => {
-    console.log(err)
-  });
+        likes: [userId],
+      },
+    }
+  )
+    .then(() => {
+      User.updateOne(
+        {
+          _id: loggedInUserId,
+        },
+        {
+          $pullAll: {
+            pending: [userId],
+          },
+        }
+      )
+        .then(() => {
+          console.log('successfully reset');
+          res.redirect('/findmatch');
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 function dislikeUser(req, res) {
-  const {
-    dislikingID
-  } = req.body;
+  const { dislikingID } = req.body;
   let dislikes = [];
   dislikes.push(dislikingID);
   const dUser = req.session.user;
-  User.findById(dUser).then((dislikingUser) => {
-    dislikingUser.dislikes.push(dislikes);
-    dislikingUser.save().then(newDislikingUser => {
-      let dislikedBy = [];
-      dislikedBy.push(dUser);
-      const userId = req.params.userId;
-      User.findById(userId).then(dislikedUser => {
-        dislikedUser.dislikedBy.push(dislikedBy);
-        dislikedUser.save().then(newDislikedUser => {
-          res.redirect("/findmatch");
-        }).catch(err => console.log(err.message));
-      }).catch(err => console.log(err.message));
-    }).catch(err => console.log(err.message));
-  }).catch(err => console.log(err));
+  User.findById(dUser)
+    .then((dislikingUser) => {
+      dislikingUser.dislikes.push(dislikes);
+      dislikingUser
+        .save()
+        .then((newDislikingUser) => {
+          let dislikedBy = [];
+          dislikedBy.push(dUser);
+          const userId = req.params.userId;
+          User.findById(userId)
+            .then((dislikedUser) => {
+              dislikedUser.dislikedBy.push(dislikedBy);
+              dislikedUser
+                .save()
+                .then((newDislikedUser) => {
+                  res.redirect('/findmatch');
+                })
+                .catch((err) => console.log(err.message));
+            })
+            .catch((err) => console.log(err.message));
+        })
+        .catch((err) => console.log(err.message));
+    })
+    .catch((err) => console.log(err));
 }
 
 function likeList(req, res) {
   const userID = req.session.user;
-  User.findById(userID).then((user) => {
-    const userMatches = [...user.matches];
-    const usersPending = [...user.pending]
-    console.log("userMatches", userMatches);
-    User.find({
-      _id: userMatches
-    }).then((matchedUsers) => {
+  User.findById(userID)
+    .then((user) => {
+      const userMatches = [...user.matches];
+      const usersPending = [...user.pending];
+      console.log('userMatches', userMatches);
       User.find({
-        _id: usersPending
-      }).then((pendingUsers) => {
-        res.render('likelist', {
-          title: "My Like list",
-          matchedUsers,
-          pendingUsers
-        });
-      }).catch(err => console.log(err));
-    }).catch(err => err.message);
-  }).catch(err => console.log(err));
+        _id: userMatches,
+      })
+        .then((matchedUsers) => {
+          User.find({
+            _id: usersPending,
+          })
+            .then((pendingUsers) => {
+              res.render('likelist', {
+                title: 'My Like list',
+                matchedUsers,
+                pendingUsers,
+              });
+            })
+            .catch((err) => console.log(err));
+        })
+        .catch((err) => err.message);
+    })
+    .catch((err) => console.log(err));
 }
 
 function profile(req, res) {
   const userID = req.session.user;
-  User.findById(userID).then((user) => {
-    res.render('myprofile', {
-      title: 'My profile',
-      user,
-    });
-  }).catch((err) => console.log(err));
+  User.findById(userID)
+    .then((user) => {
+      res.render('myprofile', {
+        title: 'My profile',
+        user,
+      });
+    })
+    .catch((err) => console.log(err));
 }
 
 app.use(function (_req, res, next) {
-  res.status(404).send("Sorry can't find that.")
-})
+  res.status(404).send("Sorry can't find that.");
+});
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
-})
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
--- public/scripts/findmatch.js
+++ public/scripts/findmatch.js
@@ -3,8 +3,8 @@ let allButtons = document.querySelectorAll('.buttons');
 
 function displayMain() {
   for (let i = 1; i < allElements.length; i++) {
-    allElements[i].style.display = "none";
-    allButtons[i].style.display = "none"
+    allElements[i].style.display = 'none';
+    allButtons[i].style.display = 'none';
   }
 }
 displayMain();
