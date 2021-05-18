a991588d1ec1e8ef493082fb74a2a80d49865426
Jabir Tisoudali <jabirtisou@hotmail.com>
2021-05-18 22:42:55 +0200

    Comments in code

--- public/scripts/findmatch.js
+++ public/scripts/findmatch.js
@@ -1,6 +1,7 @@
 let allElements = document.querySelectorAll('.match');
 let allButtons = document.querySelectorAll('.buttons');
 
+// Laat alleen de eerste user zien
 function displayMain() {
   for (let i = 1; i < allElements.length; i++) {
     allElements[i].style.display = 'none';
