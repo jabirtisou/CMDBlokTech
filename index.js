439477730dcfd49cadb84b60dc0214abbbc3a106
Jabir Tisoudali <jabirtisou@hotmail.com>
2021-06-14 16:25:10 +0200

    added progressive enhancement

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
