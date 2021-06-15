6d3e124cbbdea714ae9bc2fa1703a463d5da66c5
Jabir Tisoudali <jabirtisou@hotmail.com>
2021-06-15 15:01:08 +0200

    Images fix

--- public/scripts/findmatch.js
+++ public/scripts/findmatch.js
@@ -6,8 +6,8 @@ const allButtons = document.querySelectorAll(".buttons");
 
 function displayMain() {
   for (let i = 1; i < allElements.length; i++) {
-    allElements[i].style.display = "none";
-    allButtons[i].style.display = "none";
+    allElements[i].classList.add("hidden");
+    allButtons[i].classList.add("hidden");
   }
 }
 displayMain();
