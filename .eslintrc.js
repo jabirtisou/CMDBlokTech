1eb3f863ccb765990dae08102e2e6040637ca082
Jabir Tisoudali <jabirtisou@hotmail.com>
2021-06-22 01:29:53 +0200

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,15 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: ['airbnb-base'],
-  parserOptions: {
-    ecmaVersion: 12,
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: "es5",
+        singleQuote: true,
+        printWidth: 120,
+        tabWidth: 4,
+      },
+    ],
   },
-  rules: {},
 };
